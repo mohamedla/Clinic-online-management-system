32cde52b7ef4a56e7092c53eebd92fbbbcb6a754
mohamedla <45569538+mohamedla@users.noreply.github.com>
2020-06-17 11:36:44 -0700

    Add files via upload

---
+++ content/application/logs/log-2020-06-12.php
@@ -0,0 +1,26 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-06-12 21:09:15 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'clinic' C:\xampp\htdocs\CMS\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2020-06-12 21:09:15 --> Unable to connect to the database
+ERROR - 2020-06-12 21:09:50 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'clinic' C:\xampp\htdocs\Clinic-Management-System-master\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2020-06-12 21:09:50 --> Unable to connect to the database
+ERROR - 2020-06-12 21:12:24 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:22 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:24 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:25 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:25 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:25 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:25 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:25 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:25 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:15:27 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:28 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:30 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:31 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:31 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:31 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:31 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:31 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:31 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:17:32 --> You must enable php_gmp to use Bitauth.
+ERROR - 2020-06-12 21:41:56 --> You must enable php_gmp to use Bitauth.
